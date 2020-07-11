f3a79c680cd4b0449498e589867af317135355c0
Luiz Américo <luizamericop@gmail.com>
2020-07-11 15:12:44 -0300

    Replace @babel/register by esm

--- test/setup.js
+++ test/setup.js
@@ -1,15 +1,3 @@
-require('@babel/register')({
-  // This will override `node_modules` ignoring
-  ignore: [
-    function (filepath) {
-      const result =
-        filepath.indexOf('node_modules') !== -1 &&
-        !filepath.match(/(lodash-es|nextbone)/)
-      return result
-    },
-  ],
-})
-
 var chai = require('chai')
 var sinonChai = require('sinon-chai')
 
