5be5a7aad2a28e133adc411aac6ef661b284fef5
Luiz Américo <camara_luiz@yahoo.com.br>
2025-04-23 14:45:57 +0000

    Model: add observe featue

--- src/model.js
+++ src/model.js
@@ -2,9 +2,23 @@ import { doc, getDoc, setDoc, updateDoc, deleteDoc } from 'firebase/firestore'
 import { Model } from 'nextbone'
 
 /**
+ * @import {Firestore, DocumentReference, CollectionReference, Query, FirestoreDataConverter} from 'firebase/firestore'
+ */
+
+const createParamsProxy = (params, instance) => {
+  return new Proxy(params, {
+    set(target, prop, value) {
+      target[prop] = value
+      instance.updateRef()
+      return true
+    },
+  })
+}
+
+/**
  * @param {Model} model
  * @param {string} method
- * @returns
+ * @returns {DocumentReference | CollectionReference | undefined}
  */
 const getDocRef = (model, method) => {
   if (method === 'create') {
@@ -15,6 +29,211 @@ const getDocRef = (model, method) => {
 }
 
 class FireModel extends Model {
+  /**
+   * @returns {Firestore}
+   */
+  static get db() {
+    if (!this._db) {
+      const dbFactory = this.getDb || getFirestore
+      this._db = dbFactory()
+    }
+    return this._db
+  }
+
+  /**
+   * @type {() => Firestore}
+   */
+  static getDb
+
+  /**
+   * @type {FirestoreDataConverter}
+   */
+  static converter
+
+  constructor(attributes, options) {
+    super(attributes, options)
+    this._params = {}
+    this._paramsProxy = createParamsProxy(this._params, this)
+    this._unsubscribe = undefined
+    this.readyPromise = Promise.resolve()
+    this.updateRefPromise = undefined
+  }
+
+  get params() {
+    return this._paramsProxy
+  }
+
+  set params(value) {
+    if (!value || typeof value !== 'object') {
+      throw new Error(`FireCollection: params should be an object`)
+    }
+
+    this._params = value
+    this._paramsProxy = createParamsProxy(value, this)
+    this.updateRef()
+  }
+
+  observe() {
+    if (this._unsubscribe) {
+      this._unsubscribe()
+      this._unsubscribe = undefined
+    }
+
+    const ref = this.getRef()
+    if (!ref) {
+      return
+    }
+
+    this.changeLoadingState(true)
+    this._unsubscribe = onSnapshot(ref, (snapshot) => {
+      const docSnapshot =
+        ref.type === 'document' ? snapshot : this.selectSnapshot(snapshot)
+      if (!docSnapshot) {
+        this.clear()
+        return
+      }
+      if (docSnapshot.exists()) {
+        this.set({ id: docSnapshot.id, ...docSnapshot.data() }, { reset: true })
+      } else {
+        this.clear()
+      }
+      this.changeLoadingState(false)
+    })
+  }
+
+  /**
+   * @param {QuerySnapshot} snapshot
+   * @returns {DocumentSnapshot | undefined}
+   */
+  selectSnapshot(snapshot) {
+    return snapshot.docs[0]
+  }
+
+  /**
+   * @param {DocumentReference} ref
+   * @param {Record<string, any>} params
+   * @returns {undefined}
+   */
+  query(ref, params) {}
+
+  /**
+   * @param {Record<string, any>} params
+   * @returns {string | undefined}
+   */
+  rootPath(params) {}
+
+  /**
+   * @returns {DocumentReference | undefined}
+   */
+  getRef() {
+    const params = this._params
+    const path = this.rootPath(params)
+    if (!path) {
+      return undefined
+    }
+
+    /**
+     * @type {{db: Firestore, converter: FirestoreDataConverter}}
+     */
+    const { db, converter } = this.constructor
+    const rootRef = collection(db, path).withConverter(converter)
+
+    if (!params.id) {
+      return this.query(rootRef, params)
+    }
+
+    return doc(rootRef, params.id)
+  }
+
+  async updateRef() {
+    if (!this.updateRefPromise) {
+      // by default batch reset calls
+      this.updateRefPromise = Promise.resolve()
+      await this.updateRefPromise
+      this.changeRef(this.getRef())
+      this.updateRefPromise = undefined
+    }
+    return this._ref
+  }
+
+  /**
+   * @param {DocumentReference} newRef
+   * @returns
+   */
+  changeRef(newRef) {
+    if (!this._ref && !newRef) {
+      return
+    }
+    if (this._ref && newRef && queryEqual(this._ref, newRef)) {
+      return
+    }
+
+    if (this._unsubscribe) {
+      this._unsubscribe()
+      this._unsubscribe = undefined
+    }
+
+    if (newRef) {
+      this.changeLoadingState(true)
+      this._unsubscribe = onSnapshot(newRef, (snapshot) => {
+        const docSnapshot =
+          newRef.type === 'document' ? snapshot : this.selectSnapshot(snapshot)
+        if (!docSnapshot) {
+          this.clear()
+          return
+        }
+
+        if (docSnapshot.exists()) {
+          this.set(
+            { id: docSnapshot.id, ...docSnapshot.data() },
+            { reset: true }
+          )
+        } else {
+          this.clear()
+        }
+        this.changeLoadingState(false)
+      })
+    } else {
+      this.clear()
+      this.changeLoadingState(false)
+    }
+  }
+
+  changeReady(isReady) {
+    if (isReady) {
+      const readyResolve = this.readyResolveFn
+      if (readyResolve) {
+        this.readyResolveFn = undefined
+        readyResolve()
+      }
+    } else {
+      this.initReadyResolver()
+    }
+  }
+
+  initReadyResolver() {
+    if (!this.readyResolveFn) {
+      this.readyPromise = new Promise((resolve) => {
+        this.readyResolveFn = resolve
+      })
+    }
+  }
+
+  changeLoadingState(isLoading) {
+    if (this.isLoading === isLoading) {
+      return
+    }
+    this.changeReady(!isLoading)
+    this.isLoading = isLoading
+  }
+
+  async ready() {
+    if (this.updateRefPromise) {
+      await this.updateRefPromise
+    }
+
+    return this.readyPromise
+  }
   /**
    * @return {Promise<void> | undefined}
    */
